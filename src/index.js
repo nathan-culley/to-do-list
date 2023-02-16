af122eff3d5b672255c9a16a3c1f567a84ad5968
Nathan Culley <nathan@nathanculley.com>
2023-02-16 11:15:33 -0500

    Clean up index.js
    Removed all previously commented-out code, as well as all the initial projects and tasks (besides the default project) that were manually generated to test functionality.

--- src/index.js
+++ src/index.js
@@ -8,43 +8,5 @@ const project0 = new Project('Default Project', 'Your tasks will go here by defa
 
 projects.unshift(project0);
 
-const task1 = new Task('Task 1', 'First task', '2023-03-01', 'High', 'Important task');
-
-project0.addTask(task1);
-
-// function createProject(title, description, dueDate, notes) {
-//     const projectName = `project${projects.length}`;
-//     window[projectName] = new Project(title, description, dueDate, notes);
-//     projects.push(window[projectName]);
-// }
-
-// function createTask(project, title, description, dueDate, priority, notes) {
-//     const taskName = `task${project.taskList.length}`;
-//     window[taskName] = new Task(title, description, dueDate, priority, notes);
-//     project.taskList.push(window[taskName]);
-//   }
-
-// function markProjectAsComplete(project) {
-//     project.completed = true;
-// }
-
-// function markTaskAsComplete(task) {
-//     task.completed = true;
-// }
-
-
-createProject('Project 2', 'My second project', '2023-03-02', 'Medium');
-createTask(projects[0], 'Task 1', 'My first task', '2023-03-03', 'High', 'Important');
-
-createTask(projects[1], 'Task 1', 'My first task', '2023-03-03', 'High', 'Important');
-
-console.log(projects);
-
-// markTaskAsComplete(project0.taskList[0]);
-
-// markProjectAsComplete(projects[0]);
-
-// console.log(projects);
-
 Ui();
 

c05aaa515bf22d83e9f5d6e4fbbb1694b4167ce4
Nathan Culley <nathan@nathanculley.com>
2023-02-22 19:14:41 -0500

    Modify publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: './dist',
+    publicPath: './',
   },
   optimization: {
     runtimeChunk: 'single',
